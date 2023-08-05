fdc8030e2ee40995984d8b47ee26474eeb8fac8b
Jon Nordby <jononor@gmail.com>
2023-08-05 20:48:18 +0200

    Start implementing Nearest Neighbors

---
+++ emlneighbors/neighbors.c
@@ -0,0 +1,54 @@
+
+#define EML_LOG_ENABLE 1
+#include <eml_neighbors.h>
+
+
+#define N_FEATURES 3
+#define MAX_ITEMS 100
+#define DATA_LENGTH (MAX_ITEMS*N_FEATURES)
+
+// FIXME: move into emlearn as a test
+int
+main(int argc, char *argv[])
+{
+    // Allocate space
+    int16_t data[DATA_LENGTH];
+    int16_t labels[MAX_ITEMS];
+    EmlNeighborsDistanceItem distances[MAX_ITEMS];
+
+    const int K_NEIGHBORS = 1;
+
+    // Setup model
+    EmlNeighbors _model = { N_FEATURES, 0, MAX_ITEMS, data, labels, K_NEIGHBORS };
+    EmlNeighbors *model = &_model;
+
+    EmlError check_err = eml_neighbors_check(model, DATA_LENGTH, MAX_ITEMS, MAX_ITEMS);
+    if (check_err != EmlOk) {
+        return -1;
+    }
+
+    // Add data
+    for (int i=0; i<MAX_ITEMS; i++) {
+        const int16_t features[N_FEATURES] = { 0, 0, 0 };
+        const int16_t label = 2;
+        EmlError add_err = eml_neighbors_add_item(model, features, N_FEATURES, label);
+        if (add_err != EmlOk) {
+            return -2;
+        }
+    }
+
+    // Use the model
+
+    const int16_t features[N_FEATURES] = { 0, 0, 0 };
+
+    int16_t label = -1;
+    EmlError predict_err = \
+        eml_neighbors_predict(model, features, N_FEATURES, distances, MAX_ITEMS, &label);
+    if (predict_err != EmlOk) {
+        return -3;
+    }
+
+    printf("prediction label=%d \n", label);
+
+    return 0;
+}
