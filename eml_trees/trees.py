f68874edcbb00a65461f14ce124ad8109962d03f
Jon Nordby <jononor@gmail.com>
2023-08-11 21:47:21 +0200

    trees: Include load_model in the module

---
+++ eml_trees/trees.py
@@ -0,0 +1,21 @@
+
+
+def load_model(builder, f):
+
+    for line in f:
+        line = line.rstrip('\r')
+        line = line.rstrip('\n')
+        tok = line.split(',')
+        kind = tok[0]
+        if kind == 'r':
+            root = int(tok[1])
+            addroot(builder, root)
+        elif kind == 'n':
+            feature = int(tok[1])
+            value = float(tok[2])
+            left = int(tok[3])
+            right = int(tok[4])
+            addnode(builder, left, right, feature, value)
+        else:        
+            # unknown value
+            pass
--- examples/xor_run.py
+++ examples/xor_run.py
@@ -3,32 +3,12 @@
 import emltrees
 import array
 
-def load_model(builder, f):
-
-    for line in f:
-        line = line.rstrip('\r')
-        line = line.rstrip('\n')
-        tok = line.split(',')
-        kind = tok[0]
-        if kind == 'r':
-            root = int(tok[1])
-            emltrees.addroot(builder, root)
-        elif kind == 'n':
-            feature = int(tok[1])
-            value = float(tok[2])
-            left = int(tok[3])
-            right = int(tok[4])
-            emltrees.addnode(builder, left, right, feature, value)
-        else:        
-            # unknown value
-            pass
-
 # create
 builder = emltrees.open(5, 30)
 
 # Load a CSV file with the model
 with open('xor_model.csv', 'r') as f:
-    load_model(builder, f)
+    emltrees.load_model(builder, f)
 
 # run it
 examples = [
