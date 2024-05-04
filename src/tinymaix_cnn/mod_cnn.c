1dce24cb1786a60f1558b84495e8049d153cc357
Jon Nordby <jononor@gmail.com>
2024-05-04 17:51:33 +0200

    cnn: Fix linking for Xtensawin/ESP32

--- src/tinymaix_cnn/mod_cnn.c
+++ src/tinymaix_cnn/mod_cnn.c
@@ -13,6 +13,23 @@
 #include <string.h>
 
 
+// memset is used by some standard C constructs
+#if !defined(__linux__)
+void *memcpy(void *dst, const void *src, size_t n) {
+    return mp_fun_table.memmove_(dst, src, n);
+}
+void *memset(void *s, int c, size_t n) {
+    return mp_fun_table.memset_(s, c, n);
+}
+
+void NORETURN abort() {
+    while (1) {
+        ;
+    }
+}
+#endif
+
+
 static tm_err_t layer_cb(tm_mdl_t* mdl, tml_head_t* lh)
 {  
 #if 0
