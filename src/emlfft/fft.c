06c67837932ad37eaf562b2063b1097435f90f05
Jon Nordby <jononor@gmail.com>
2024-05-05 12:45:02 +0200

    fft: Fill cos/sin values from Python side
    Now builds on ARCH=x64/xtensawin/armv7emsp
    Workaround for https://github.com/micropython/micropython/issues/14430

--- src/emlfft/fft.c
+++ src/emlfft/fft.c
@@ -1,7 +1,7 @@
 // Include the header file to get access to the MicroPython API
 #include "py/dynruntime.h"
 
-#include <eml_fft.h>
+#include <eml_common.h>
 
 #include <string.h>
 #include <errno.h> // used by sincosf
@@ -22,42 +22,73 @@ void NORETURN abort() {
 }
 #endif
 
-#if 0
-int errno;
 
-//void __sincosf_sse2(float x, float *sin, float *cos);
-//void __sincosf_ifunc(float x, float *sin, float *cos);
-void __sincosf_fma(float x, float *sin, float *cos);
-
-void sincosf(float x, float *sin, float *cos) {
-    //__sincosf_sse2(x, sin, cos);
-    __sincosf_fma(x, sin, cos);
+// Copy of eml_fft.h, without eml_fft_fill
+// - contains sin/cos that trips up mpy_ld.py (even if the function is not used)
+static size_t reverse_bits(size_t x, int n) {
+	size_t result = 0;
+	for (int i = 0; i < n; i++, x >>= 1)
+		result = (result << 1) | (x & 1U);
+	return result;
 }
 
-// math_errf.c
-#define with_errnof(x, e) (x)
-float __math_invalidf (float x)
-{
-  float y = (x - x) / (x - x);
-  return isnan (x) ? y : with_errnof (y, EDOM);
+/** @typedef EmlFFT
+*
+* FFT algorithm state
+*/
+typedef struct _EmlFFT {
+    int length; // (n/2)
+    float *sin;
+    float *cos;
+} EmlFFT;
+
+
+EmlError
+eml_fft_forward(EmlFFT table, float real[], float imag[], size_t n) {
+
+    // Compute levels = floor(log2(n))
+	int levels = 0;
+	for (size_t temp = n; temp > 1U; temp >>= 1)
+		levels++;
+
+    EML_PRECONDITION(((size_t)(1U << levels)) == n, EmlSizeMismatch);
+    EML_PRECONDITION((size_t)table.length == n/2, EmlSizeMismatch);
+
+	// Bit-reversed addressing permutation
+	for (size_t i = 0; i < n; i++) {
+		size_t j = reverse_bits(i, levels);
+		if (j > i) {
+			float temp = real[i];
+			real[i] = real[j];
+			real[j] = temp;
+			temp = imag[i];
+			imag[i] = imag[j];
+			imag[j] = temp;
+		}
+	}
+
+	// Cooley-Tukey decimation-in-time radix-2 FFT
+	for (size_t size = 2; size <= n; size *= 2) {
+		size_t halfsize = size / 2;
+		size_t tablestep = n / size;
+		for (size_t i = 0; i < n; i += size) {
+			for (size_t j = i, k = 0; j < i + halfsize; j++, k += tablestep) {
+				size_t l = j + halfsize;
+				float tpre =  real[l] * table.cos[k] + imag[l] * table.sin[k];
+				float tpim = -real[l] * table.sin[k] + imag[l] * table.cos[k];
+				real[l] = real[j] - tpre;
+				imag[l] = imag[j] - tpim;
+				real[j] += tpre;
+				imag[j] += tpim;
+			}
+		}
+		if (size == n)  // Prevent overflow in 'size *= 2'
+			break;
+	}	
+	return EmlOk;
 }
 
-//     glibc/sysdeps/ieee754/flt-32/s_sincosf_data.c
 
-
-/* Table with 4/PI to 192 bit precision.  To avoid unaligned accesses
-   only 8 new bits are added per entry, making the table 4 times larger.  */
-const uint32_t __inv_pio4[24] =
-{
-  0xa2,       0xa2f9,	  0xa2f983,   0xa2f9836e,
-  0xf9836e4e, 0x836e4e44, 0x6e4e4415, 0x4e441529,
-  0x441529fc, 0x1529fc27, 0x29fc2757, 0xfc2757d1,
-  0x2757d1f5, 0x57d1f534, 0xd1f534dd, 0xf534ddc0,
-  0x34ddc0db, 0xddc0db62, 0xc0db6295, 0xdb629599,
-  0x6295993c, 0x95993c43, 0x993c4390, 0x3c439041
-};
-
-#endif
 
 // MicroPython type for EmlFFT
 typedef struct _mp_obj_fft_t {
@@ -74,19 +105,23 @@ static mp_obj_t fft_new(mp_obj_t length_obj) {
 
     // Construct object
     mp_obj_fft_t *o = mp_obj_malloc(mp_obj_fft_t, (mp_obj_type_t *)&fft_type);
-    EmlFFT self = o->fft;
-    //memset(self, 0, sizeof(EmlIIR)); // HACK: try to get memset symbol in
+    EmlFFT *self = &o->fft;
 
     const int table_length = fft_length / 2;
-    self.cos = (float *)m_malloc(sizeof(float)*table_length);
-    self.sin = (float *)m_malloc(sizeof(float)*table_length);
+    self->cos = (float *)m_malloc(sizeof(float)*table_length);
+    self->sin = (float *)m_malloc(sizeof(float)*table_length);
 
+#if 0
+    // Using sinf/cosf does not work with mpy_ld.py
+    // https://github.com/micropython/micropython/issues/14430
+    // As a workaround, we require the FFT factors to be filled from the MicroPython side
     const EmlError err = eml_fft_fill(self, fft_length);
     if (err != EmlOk) {
         m_free(self.cos);
         m_free(self.sin);
         mp_raise_ValueError(MP_ERROR_TEXT("EmlError"));
     }
+#endif
 
     return MP_OBJ_FROM_PTR(o);
 }
@@ -96,11 +131,11 @@ static MP_DEFINE_CONST_FUN_OBJ_1(fft_new_obj, fft_new);
 static mp_obj_t fft_del(mp_obj_t self_obj) {
 
     mp_obj_fft_t *o = MP_OBJ_TO_PTR(self_obj);
-    EmlFFT self = o->fft;
+    EmlFFT *self = &o->fft;
 
     // free allocated data
-    m_free(self.cos);
-    m_free(self.sin);
+    m_free(self->cos);
+    m_free(self->sin);
 
     return mp_const_none;
 }
@@ -128,17 +163,34 @@ check_extract_array(mp_obj_t obj, int length) {
     return values;
 }
 
-// Add a node to the tree
+// Fill the FFT factors
+static mp_obj_t fft_fill(mp_obj_t self_obj, mp_obj_t sin_obj, mp_obj_t cos_obj) {
+
+    mp_obj_fft_t *o = MP_OBJ_TO_PTR(self_obj);
+    EmlFFT *self = &o->fft;
+    const int length = self->length;
+
+    float *sin_values = check_extract_array(sin_obj, length);
+    float *cos_values = check_extract_array(cos_obj, length);
+
+    memcpy(self->sin, sin_values, sizeof(float)*length);
+    memcpy(self->cos, cos_values, sizeof(float)*length);
+
+    return mp_const_none;
+ }
+static MP_DEFINE_CONST_FUN_OBJ_3(fft_fill_obj, fft_fill);
+
+// Compute the FFT
 static mp_obj_t fft_run(mp_obj_t self_obj, mp_obj_t real_obj, mp_obj_t imag_obj) {
 
     mp_obj_fft_t *o = MP_OBJ_TO_PTR(self_obj);
-    EmlFFT self = o->fft;
-    const int fft_length = self.length*2;
+    EmlFFT *self = &o->fft;
+    const int fft_length = self->length*2;
 
     float *real_values = check_extract_array(real_obj, fft_length);
     float *imag_values = check_extract_array(imag_obj, fft_length);
 
-    const EmlError err = eml_fft_forward(self, real_values, imag_values, fft_length);
+    const EmlError err = eml_fft_forward(*self, real_values, imag_values, fft_length);
     if (err != EmlOk) {
         mp_raise_msg(&mp_type_RuntimeError, MP_ERROR_TEXT("eml_fft_forward error"));
     }
@@ -148,8 +200,8 @@ static mp_obj_t fft_run(mp_obj_t self_obj, mp_obj_t real_obj, mp_obj_t imag_obj)
 static MP_DEFINE_CONST_FUN_OBJ_3(fft_run_obj, fft_run);
 
 
-mp_map_elem_t fft_locals_dict_table[2];
-static MP_DEFINE_CONST_DICT(fft_locals_dict, fft_locals_dict_table);
+mp_map_elem_t mod_locals_dit_table[3];
+static MP_DEFINE_CONST_DICT(mod_locals_dit, mod_locals_dit_table);
 
 // This is the entry point and is called when the module is imported
 mp_obj_t mpy_init(mp_obj_fun_bc_t *self, size_t n_args, size_t n_kw, mp_obj_t *args) {
@@ -162,10 +214,11 @@ mp_obj_t mpy_init(mp_obj_fun_bc_t *self, size_t n_args, size_t n_kw, mp_obj_t *a
     fft_type.flags = MP_TYPE_FLAG_ITER_IS_CUSTOM;
     fft_type.name = MP_QSTR_emlfft;
     // methods
-    fft_locals_dict_table[0] = (mp_map_elem_t){ MP_OBJ_NEW_QSTR(MP_QSTR_run), MP_OBJ_FROM_PTR(&fft_run_obj) };
-    fft_locals_dict_table[1] = (mp_map_elem_t){ MP_OBJ_NEW_QSTR(MP_QSTR___del__), MP_OBJ_FROM_PTR(&fft_del_obj) };
+    mod_locals_dit_table[0] = (mp_map_elem_t){ MP_OBJ_NEW_QSTR(MP_QSTR_run), MP_OBJ_FROM_PTR(&fft_run_obj) };
+    mod_locals_dit_table[1] = (mp_map_elem_t){ MP_OBJ_NEW_QSTR(MP_QSTR___del__), MP_OBJ_FROM_PTR(&fft_del_obj) };
+    mod_locals_dit_table[2] = (mp_map_elem_t){ MP_OBJ_NEW_QSTR(MP_QSTR_fill), MP_OBJ_FROM_PTR(&fft_fill_obj) };
 
-    MP_OBJ_TYPE_SET_SLOT(&fft_type, locals_dict, (void*)&fft_locals_dict, 2);
+    MP_OBJ_TYPE_SET_SLOT(&fft_type, locals_dict, (void*)&mod_locals_dit, 2);
 
     // This must be last, it restores the globals dict
     MP_DYNRUNTIME_INIT_EXIT
---
+++ src/emlfft/fft.py
@@ -0,0 +1,21 @@
+
+import math
+import gc
+
+def fill(fft, n):
+    # pre-compute the trigonometrics needed for FFT computation
+    
+    coeffs = n // 2
+
+    # pre-compute the constant part of expression
+    PI2_N = (math.pi * 2.0) / N
+
+    # compute coeffeicients
+    sin = array.array('f', (math.sin(PI2_N * i) for i in range(coeffs)) )
+    cos = array.array('f', (math.cos(PI2_N * i) for i in range(coeffs)) )
+
+    # pass it to the C module
+    fft.fill(sin, cos)
+
+    # make sure temporary arrays get freed
+    gc.free()
