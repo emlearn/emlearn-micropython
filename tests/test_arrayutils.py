681f12c9b195b52241b6aaf9804820e6755e875c
Jon Nordby <jononor@gmail.com>
2024-10-07 21:16:17 +0200

    arrayutils: Fix test to cover negative range

--- tests/test_arrayutils.py
+++ tests/test_arrayutils.py
@@ -1,4 +1,5 @@
 
+import time
 import math
 import array
 
@@ -14,26 +15,30 @@ def assert_almost_equal(a, b, rtol=0.001, errortol=0):
 
     errors = 0
     for x, y in zip(a, b):
-        rdiff = (abs(x - y) / (y+0.00001))
+        rdiff = abs((x - y) / (y+0.00001))
         if rdiff > rtol:
             errors += 1
 
-    assert errors > errortol, (errors)
+    assert errors <= errortol, (errors)
 
 
-def test_arrayutils_linear_map_int16():
+def test_arrayutils_linear_map_float_int16():
 
     length = 2000
-    int16 = array.array('h', range(length))
+    int16 = array.array('h', (i-length//2 for i in range(length)))
     flt = array.array('f', (0 for _ in range(length)))
     out = array.array('h', (0 for _ in range(length)))
 
     # roundtrip should be (approx) equal
-    linear_map(int16, flt, 0, 2**15, 0.0, 1.0)
-    linear_map(flt, out, 0, 1.0, 0, 2**15)
-    assert_almost_equal(out, int16) 
+    start = time.ticks_us()
+    linear_map(int16, flt, -2**15, 2**15, -1.0, 1.0)
+    linear_map(flt, out, -1.0, 1.0, -2**15, 2**15)
+    d = time.ticks_diff(time.ticks_us(), start)
+    print('linear_map int16>float>int16', length, d/1000.0)
+    assert_almost_equal(out, int16)
 
-test_arrayutils_linear_map_int16()
+
+test_arrayutils_linear_map_float_int16()
 
 # Other functionality
 # reinterpret
