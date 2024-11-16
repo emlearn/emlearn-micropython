ecfcf03800d7877db60e1c67366bf5c7000fbfa4
Jon Nordby <jononor@gmail.com>
2024-11-16 17:51:17 +0100

    har_trees: Data recorder can start/stop

--- examples/har_trees/har_record.py
+++ examples/har_trees/har_record.py
@@ -1,12 +1,17 @@
 
 
+import machine
 from machine import Pin, I2C
 from mpu6886 import MPU6886
 import npyfile
 
+import os
 import time
 import array
 import struct
+import gc
+
+gc.collect()
 
 def format_time(secs):
     year, month, day, hour, minute, second, _, _ = time.gmtime(secs)
@@ -20,9 +25,7 @@ classes = [
     'pushup',
 ]
 
-samplerate = 100
-chunk_length = 50
-file_duration = 10.0
+
 
 def decode_samples(buf : bytearray, samples : array.array, bytes_per_sample):
     """
@@ -40,6 +43,84 @@ def decode_samples(buf : bytearray, samples : array.array, bytes_per_sample):
         samples[(i*3)+1] = y
         samples[(i*3)+2] = z
 
+def file_or_dir_exists(filename):
+    try:
+        os.stat(filename)
+        return True
+    except OSError:
+        return False
+
+class Recorder():
+    """Record accelerometer data to .npy files"""
+    
+    def __init__(self, samplerate, duration, directory='recorder_data', suffix='.npy'):
+        # config      
+        self._directory = directory
+        assert directory[-1] != '/'
+        self._suffix  = suffix
+        self._recording_samples = int(duration * samplerate)
+
+        # state
+        self._recording_file = None
+        self._recording = False
+
+        if not file_or_dir_exists(self._directory):
+            os.mkdir(self._directory)
+
+    def start(self):
+        self._recording = True
+
+    def stop(self):
+        self.close()
+        self._recording = False
+
+    def process(self, data):
+
+        if not self._recording:
+            return
+
+        t = time.ticks_ms()/1000.0
+
+        if self._recording_file is None:
+            # open file
+            time_str = format_time(time.time())
+            out_path = f'{self._directory}/{time_str}_{self._suffix}'
+            out_typecode = 'h'
+            out_shape = (3, self._recording_samples)
+            self._recording_file = npyfile.Writer(open(out_path, 'w'), out_shape, out_typecode)
+            print(f'record-file-open t={t:.3f} file={out_path}')
+
+        # TODO: avoid writing too much at end of file
+        self._recording_file.write_values(data)
+        print(f'record-chunk t={t:.3f}')
+        if self._recording_file.written_bytes > 3*2*self._recording_samples:
+            # rotate file
+            self.close()
+            print(f'record-file-rotate t={t:.3f}')
+
+    def delete(self):
+        for f in os.listdir(self._directory):
+            p = self._directory + '/' + f
+            print('recorder-delete-file', p)
+            os.unlink(p)
+
+    def close(self):
+        if self._recording_file is not None:
+            self._recording_file.close()
+            self._recording_file = None
+
+    # Support working as a context manager, to automatically clean up
+    def __enter__(self):
+        pass      
+        return self
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.close()
+
+# Configuration
+samplerate = 100
+chunk_length = 50
+file_duration = 5.0
+data_dir = 'har_record'
 
 def main():
     mpu = MPU6886(I2C(0, sda=21, scl=22, freq=100000))
@@ -48,56 +129,53 @@ def main():
     mpu.fifo_enable(True)
     mpu.set_odr(samplerate)
 
-    chunk = bytearray(mpu.bytes_per_sample*chunk_length)
-    decoded = array.array('h', (0 for _ in range(3*chunk_length)))
-    recording_file = None
-    recording_samples = int(file_duration * samplerate)
-
-    # TODO: support start/stop of recording
-    recording = True
+    chunk = bytearray(mpu.bytes_per_sample*chunk_length) # raw bytes
+    decoded = array.array('h', (0 for _ in range(3*chunk_length))) # decoded int16
 
     # TODO: support specifying class being recorded
     current_class = classes[0]
 
-    start_time = time.ticks_ms()
+    led_pin = machine.Pin(19, machine.Pin.OUT) # Internal LED on M5StickC PLUS2
+    button_pin = machine.Pin(37, machine.Pin.IN, machine.Pin.PULL_UP) # Button A on M5StickC PLUS2
 
-    while True:
+    # On M5StickC we need to set HOLD pin to stay alive when on battery
+    hold_pin = machine.Pin(4, machine.Pin.OUT)
+    hold_pin.value(1)
 
-        try:
+    with Recorder(samplerate, file_duration, directory=data_dir) as recorder:
 
+        # UNCOMMENT to clean up data_dir
+        recorder.delete()
+    
+        print('har-record-ready')
+
+        while True:
+        
             # always read data from FIFO, to avoid overflow
             count = mpu.get_fifo_count()
             if count >= chunk_length:
                 start = time.ticks_ms()
                 mpu.read_samples_into(chunk)
                 decode_samples(chunk, decoded, mpu.bytes_per_sample)
-                t = (time.ticks_ms() - start_time)/1000.0
 
-                if recording:
-                    # store the data
-                    if recording_file is None:
-                        time_str = format_time(time.time())
-                        out_path = f'har_{time_str}_{current_class}.npy'
-                        out_typecode = 'h'
-                        out_shape = (3, recording_samples)
-                        recording_file = npyfile.Writer(open(out_path, 'w'), out_shape, out_typecode)
-                        print(f'record-file-open t={t:.3f} file={out_path}')
+                # record data (if enabled)
+                recorder.process(decoded)
 
-                    recording_file.write_values(decoded)
-                    print(f'record-chunk t={t:.3f}')
-                    if recording_file.written_bytes > 3*2*recording_samples:
-                        # rotate file
-                        recording_file.close()
-                        recording_file = None
-                        print(f'record-file-rotate t={t:.3f}')
+            # check button for start/stop
+            button_pressed = button_pin.value() == 0
+            if button_pressed:
+                # toggle recording state
+                if recorder._recording:
+                    recorder.stop()
+                else:
+                    recorder.start()
 
-            time.sleep_ms(1)
+            # Let LED reflect recording state        
+            led_pin.value(1 if recorder._recording else 0)
 
-        except Exception as e:
-            if recording_file is not None:
-                recording_file.close()
-                recording_file = None
-            raise e
+
+            time.sleep_ms(10)
+
 
 
 if __name__ == '__main__':
