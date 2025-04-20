f317279635840abdf306011db920ae507f54f753
Jon Nordby <jononor@gmail.com>
2025-04-21 00:35:56 +0200

    xor_run: Fix example after emlearn_trees predict() change
    Must pass output array

--- examples/xor_trees/xor_run.py
+++ examples/xor_trees/xor_run.py
@@ -3,6 +3,16 @@
 import emlearn_trees
 import array
 
+def argmax(arr):
+    idx_max = 0
+    value_max = arr[0]
+    for i in range(1, len(arr)):
+        if arr[i] > value_max:
+            value_max = arr[i]
+            idx_max = i
+
+    return idx_max
+
 model = emlearn_trees.new(5, 30, 2)
 
 # Load a CSV file with the model
@@ -18,7 +28,9 @@ examples = [
     array.array('h', [max_val, 0]),
 ]
 
-for ex in examples: 
-    result = model.predict(ex)
+out = array.array('f', range(model.outputs()))
+for ex in examples:
+    model.predict(ex, out)
+    result = argmax(out)
     print(ex, result)
 
