f57bba4b52b2099b5535d9c000405e49a6d7405b
Jon Nordby <jononor@gmail.com>
2025-08-13 20:11:51 +0200

    emlearn_iir_q15: Try avoid duplicate symbol on RISC-V

--- src/emlearn_iir_q15/iir_filter.c
+++ src/emlearn_iir_q15/iir_filter.c
@@ -5,12 +5,13 @@
 
 #include <string.h>
 
-// memset is used by some standard C constructs
-#if !defined(__linux__)
-__attribute__((weak)) void *memcpy(void *dst, const void *src, size_t n) {
+// memset is used by some standard C constructs. Missing on some platforms, but not all...
+// Unfortunately cannot use weak symbols with mpy_ld
+#if !(defined(__linux__) || defined(__riscv__))
+void *memcpy(void *dst, const void *src, size_t n) {
     return mp_fun_table.memmove_(dst, src, n);
 }
-__attribute__((weak)) void *memset(void *s, int c, size_t n) {
+void *memset(void *s, int c, size_t n) {
     return mp_fun_table.memset_(s, c, n);
 }
 #endif
