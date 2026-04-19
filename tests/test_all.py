53d054331c692c72af27532f1325a096865da371
Jon Nordby <jononor@gmail.com>
2026-04-19 22:29:18 +0200

    tests: Run everything except CNN as extmod
    This way we ensure coverage for new modules,
    even though CNN currently broken

--- tests/test_all.py
+++ tests/test_all.py
@@ -40,7 +40,15 @@ def main():
     
     modules = TEST_MODULES
     if len(sys.argv) >= 2:
-        modules = sys.argv[1].split(',') 
+        config = sys.argv[1].split(',')
+        skip = [ m[1:] for m in config if m[0] == '-' ]
+        add = [ m for m in config if m[0] != '-' ]
+        if len(skip):
+            modules = [m for m in TEST_MODULES if not m in skip ]
+            print('SKIPPING', skip)
+        if len(add):
+            modules = add
+            print('RUN ONLY', add)
 
     passed = 0
     failed = 0
